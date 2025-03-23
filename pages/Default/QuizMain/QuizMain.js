96596ccb29a37ce9dbd79b50dbe0d6326e617a7a
kwert666 <124228195+kwert666@users.noreply.github.com>
2025-03-23 20:27:33 +0800

    心理咨询主页面demo&咨询记录页面微调

--- pages/Default/QuizMain/QuizMain.js
+++ pages/Default/QuizMain/QuizMain.js
@@ -5,9 +5,41 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    questionnaires: [
+      {
+        id: 1,
+        title: "焦虑自测",
+        description: "通过10个问题评估你的焦虑水平。",
+        icon: "/images/用户.png"
+      },
+      {
+        id: 2,
+        title: "抑郁自测",
+        description: "通过10个问题评估你的抑郁水平。",
+        icon: "/images/用户.png"
+      },
+      {
+        id: 3,
+        title: "压力自测",
+        description: "通过10个问题评估你的压力水平。",
+        icon: "/images/用户.png"
+      },
+      {
+        id: 4,
+        title: "孤独自测",
+        description: "通过10个问题评估你的孤独水平。",
+        icon: "/images/用户.png"
+      }
+    ]
   },
 
+  // 处理问卷点击事件
+  handleQuestionnaireTap: function (event) {
+    const questionnaireId = event.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: `/pages/Default/Quiz/Quiz?id=${questionnaireId}`
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
